f2e4a7b3056fd1ac14b35322ded95809ce981a35
vicki <surajbarik2003@gmail.com>
2025-01-10 01:29:54 +0530

    done

--- composeApp/src/androidMain/kotlin/org/sj/cricradio/App.kt
+++ composeApp/src/androidMain/kotlin/org/sj/cricradio/App.kt
@@ -3,11 +3,19 @@ package org.sj.cricradio
 import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
 import org.jetbrains.compose.ui.tooling.preview.Preview
+import org.koin.compose.koinInject
+import org.sj.cricradio.presentation.MatchScreen
+import org.sj.cricradio.presentation.MatchViewModel
 
 @Composable
 @Preview
 fun App() {
-    MaterialTheme {
 
+    val viewModel: MatchViewModel = koinInject()
+
+    MaterialTheme {
+        MatchScreen(
+            viewModel = viewModel
+        )
     }
 }
